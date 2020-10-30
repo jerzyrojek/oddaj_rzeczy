621edd9bdcfc27f3581849af1c4fb20abefb7688
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-10-30 13:38:35 +0100

    early login page route for non-auth user

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,14 @@ import React from 'react';
 import decoration from "../assets/Decoration.svg";
 import heroImage from "../assets/HeroImage.png";
 import heroImage2 from "../assets/HeroImage@2x.png";
+import {useHistory} from "react-router-dom";
 
 const Header = () => {
+    const history = useHistory();
+    const handleButtonClicked = () => {
+        history.push("/logowanie");
+    }
+
     return (
         <header className="mainHeader container">
             <div className="mainHeader__image">
@@ -13,8 +19,8 @@ const Header = () => {
                 <h1><span>Zacznij pomagać!</span> Oddaj niechciane rzeczy w zaufane ręce</h1>
                 <img src={decoration} alt="decoration"/>
                 <div>
-                    <button>Oddaj Rzeczy</button>
-                    <button>Zorganizuj zbiorkę</button>
+                    <button onClick={handleButtonClicked}>Oddaj Rzeczy</button>
+                    <button onClick={handleButtonClicked}>Zorganizuj zbiorkę</button>
                 </div>
             </div>
         </header>
@@ -22,3 +28,4 @@ const Header = () => {
 };
 
 export default Header;
+
