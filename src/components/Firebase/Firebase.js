c67f6e6b0b45dfca03f48d8978cccfc58624cec7
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-10-31 16:31:15 +0100

    firebase configuration

---
+++ src/components/Firebase/Firebase.js
@@ -0,0 +1,37 @@
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/analytics";
+import "firebase/firestore";
+
+const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config)
+        this.auth = app.auth();
+        this.database = app.firestore();
+    }
+
+
+    newUserEmailAndPassword = (email, password) => {
+        return this.auth.createUserWithEmailAndPassword(email, password);
+    };
+
+    signInEmailAndPassword = (email, password) => {
+        return this.auth.signInWithEmailAndPassword(email, password);
+    };
+
+    signOut = () => this.auth.signOut();
+
+}
+
+export default Firebase;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./scss/main.scss";
+import Firebase from "./components/Firebase/Firebase";
+import FirebaseContext from "./components/Firebase/FirebaseContext";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <FirebaseContext.Provider value={new Firebase()}>
+            <App/>
+        </FirebaseContext.Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
