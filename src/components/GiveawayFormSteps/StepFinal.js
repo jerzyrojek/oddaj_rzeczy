f6ad3651b0d960cd6ec2e903f1970d113368c28d
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-11-08 16:16:55 +0100

    final step

---
+++ src/components/GiveawayFormSteps/StepFinal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import decoration from "../../assets/Decoration.svg";
+
+const StepFinal = () => {
+    return (
+        <>
+            <div className="giveawayForm__inputs">
+                <div className="stepFinal container">
+                    <h1>Dziękujemy za przesłanie formularza. <span>Na maila prześlemy wszelkie</span> <span>informacje o odbiorze.</span></h1>
+                    <img src={decoration} alt="decoration"/>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default StepFinal;
