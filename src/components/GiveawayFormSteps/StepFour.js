2752d84ef36f8eb31f448a5dc80285f07ee3d6ae
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-11-09 12:57:30 +0100

    step four early

--- src/components/GiveawayFormSteps/StepFour.js
+++ src/components/GiveawayFormSteps/StepFour.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const StepFour = ({prev,next, handleChange, data}) => {
+const StepFour = ({prev, next, handleChange, data}) => {
     return (
         <>
             <div className="giveawayForm__notice">
@@ -14,39 +14,55 @@ const StepFour = ({prev,next, handleChange, data}) => {
                 <div className="stepFour container">
                     <p>Krok 4/4</p>
                     <h1>Podaj adres oraz termin odbioru rzecz przez kuriera</h1>
-                    <div className="pickUpAddress">
-                        <h2>Adres odbioru</h2>
+                    <div className="stepFour__giveawayDetails">
+                        <div className="pickUpAddress">
+                            <h2>Adres odbioru:</h2>
 
-                        <label>Ulica</label>
-                        <input onChange={handleChange} value={data.street} name="street" type="text"/>
+                            <div className="inputField">
+                                <label>Ulica</label>
+                                <input onChange={handleChange} value={data.street} name="street" type="text"/>
+                            </div>
 
-                        <label>Miasto</label>
-                        <input onChange={handleChange} value={data.city} name="city" type="text"/>
+                            <div className="inputField">
+                                <label>Miasto</label>
+                                <input onChange={handleChange} value={data.city} name="city" type="text"/>
+                            </div>
 
-                        <label>Kod pocztowy</label>
-                        <input onChange={handleChange} value={data.postCode} name="postCode" type="text"/>
+                            <div className="inputField">
+                                <label>Kod pocztowy</label>
+                                <input onChange={handleChange} value={data.postCode} name="postCode" type="text"/>
+                            </div>
 
-                        <label>Numer telefonu</label>
-                        <input onChange={handleChange} value={data.phoneNumber} name="phoneNumber" type="text"/>
+                            <div className="inputField">
+                                <label>Numer telefonu</label>
+                                <input onChange={handleChange} value={data.phoneNumber} name="phoneNumber" type="text"/>
+                            </div>
 
+                        </div>
 
+                        <div className="pickUpDate">
+                            <h2>Termin odbioru:</h2>
+                            <div className="inputField">
+                                <label>Data</label>
+                                <input onChange={handleChange} value={data.date} name="date" type="date"/>
+                            </div>
+
+                            <div className="inputField">
+                                <label>Godzina</label>
+                                <input onChange={handleChange} value={data.time} name="time" type="time"/>
+                            </div>
+
+                            <div className="inputField">
+                                <label>Uwagi dla kuriera</label>
+                                <input onChange={handleChange} value={data.additionalInfo} name="additionalInfo"
+                                       type="text"/>
+                            </div>
+                        </div>
                     </div>
-
-                    <div className="pickUpDate">
-                        <h2>Termin odbioru</h2>
-
-                        <label>Data</label>
-                        <input onChange={handleChange} value={data.date} name="date" type="date"/>
-
-                        <label>Godzina</label>
-                        <input onChange={handleChange} value={data.time} name="time" type="time"/>
-
-                        <label>Uwagi dla kuriera</label>
-                        <input onChange={handleChange} value={data.additionalInfo} name="additionalInfo" type="text"/>
+                    <div>
+                        <button className="prev" onClick={prev}>Wstecz</button>
+                        <button className="next" onClick={next}>Dalej</button>
                     </div>
-
-                    <button className="prev" onClick={prev}>Wstecz</button>
-                    <button className="next" onClick={next}>Dalej</button>
                 </div>
             </div>
         </>
